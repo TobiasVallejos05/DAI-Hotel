3a8d18b9244bce100c97dd6f4f27bb351d68b090
Android Studio <devandroid@ort>
2022-08-23 08:54:06 -0300

    23/08

--- axios/axiosClient.js
+++ axios/axiosClient.js
@@ -4,7 +4,7 @@ const axiosClient = axios.create({
     baseURL: "http://challenge-react.alkemy.org/"
 })
 
-export const enviarEmailPassword = async (obj) => {
+export const validarUsuario = async (obj) =>{
     return axiosClient.post('', {
         ...obj
     }).then(response =>{
@@ -22,20 +22,13 @@ export const enviarEmailPassword = async (obj) => {
         throw err
     })
 }
-export const traerPlatos = async (query) => {
-    return axiosClient.get('?query=' + query, {
-    }).then(response => {
-        if(response.status < 300){
-            console.log(response.data)
-            return response.data
-        }
-        else {
-            console.log("Algo no funciona")
-        }
+export const traerPlatos = async (query) =>{
+    return axiosClient.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=bb614d1a2cfe4751b4f2aea0a3844a1c&query=${query}`,{})
+    .then(function(res){
+        console.log(res.data.results)
+        return res.data.results
     })
-    .catch(function(err) {
-        console.log("No funciona", err)
-        alert("No funciona, ingrese los datos correctamente")
-        throw err
+    .catch(function(){
+        throw "Error"
     })
 }
